369fec569a4a9517c69e201c4fc45cbf18c40f2d
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-11-18 18:37:02 +0300

    Sorting

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren} from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 
@@ -7,6 +7,9 @@ import { MainService } from './main.service';
 import { Paths } from '../paths';
 import { Transaction } from './transaction';
 import { UserInfo } from './user-info';
+
+export type SortColumn = keyof Transaction;
+export type SortDirection = 'asc' | 'desc';
 
 @Component({
     selector: "main",
@@ -18,6 +21,10 @@ export class MainComponent implements OnInit {
     userInfo: UserInfo = new UserInfo("", "", "", 0);
     transactions: Array<Transaction> = new Array(); 
     errorMessages = new Array();
+
+    sortColumn: SortColumn = 'date';
+    sortDirection: SortDirection = 'desc';
+    compare = (v1: string | number | Date, v2: string | number | Date) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
 
     constructor(
         private authService: AuthService,
@@ -41,15 +48,7 @@ export class MainComponent implements OnInit {
         }))
         .subscribe((data: Array<Transaction>) => {
             this.transactions = data;
-            this.transactions.sort((x, y) =>{
-                if (x.date > y.date){
-                    return -1;
-                }
-                if (x.date < y.date){
-                    return 1;
-                }
-                return 0;
-            })
+            this.sort();
         });
     }
 
@@ -66,5 +65,22 @@ export class MainComponent implements OnInit {
 
     public logout(): void {
         this.authService.logout$();
+    }
+
+    public sort(sortColumn?: SortColumn){
+        if (sortColumn !== undefined){
+            if(this.sortColumn !== sortColumn || this.sortDirection === 'desc'){
+                this.sortDirection = 'asc';
+                this.sortColumn = sortColumn;
+            }
+            else{
+                this.sortDirection = 'desc'; 
+            }
+        }
+
+        this.transactions = [...this.transactions].sort((a, b) => {
+            const res = this.compare(a[this.sortColumn], b[this.sortColumn]);
+            return this.sortDirection === 'asc' ? res : -res;
+          });
     }
 }
