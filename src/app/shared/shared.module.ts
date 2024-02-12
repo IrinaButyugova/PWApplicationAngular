794f998bc8f4a70e9c28c651b258b50da573c5c4
IrinaButyugova <irina.butyugova@sharp-dev.net>
2024-02-12 12:18:46 +0300

    Error message component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { ErrorMessageComponent } from "./error-message/error-message.component";
+
+@NgModule({
+  declarations: [ErrorMessageComponent],
+  exports: [ErrorMessageComponent],
+})
+export class SharedModule {}
