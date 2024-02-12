b77ffed63b2269ee329ff6c030792af33bee51da
IrinaButyugova <irina.butyugova@sharp-dev.net>
2024-02-12 10:28:33 +0300

    Sign in/sign up components displaying without routing

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { Observable } from "rxjs";
+
+import { isLoggedInSelector } from "./store/selectors";
 
 @Component({
   selector: "auth",
   templateUrl: "./auth.component.html",
 })
-export class AuthComponent {
-  constructor() {}
+export class AuthComponent implements OnInit {
+  isLoggedIn$!: Observable<boolean>;
+  signInActive: boolean = true;
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
+  }
+
+  signIn(): void {
+    this.signInActive = true;
+  }
+
+  signUp(): void {
+    this.signInActive = false;
+  }
 }
